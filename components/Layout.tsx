49cdd184f520a06bf7ea376494f323c633da0e06
Samuel Armitt-Fior <68110533+Sam-Archie@users.noreply.github.com>
2022-11-14 18:37:19 +0000

    basic layout and component structure complete

---
+++ components/Layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
