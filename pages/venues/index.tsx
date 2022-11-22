47b8c239d1588aac0ae0f0ceec2cc4a4a1a87b4c
Samuel Armitt-Fior <68110533+Sam-Archie@users.noreply.github.com>
2022-11-22 15:13:39 +0000

    fixing colors

--- pages/venues/index.tsx
+++ pages/venues/index.tsx
@@ -10,11 +10,8 @@ export default function Venues() {
   const { data: venueData } = useGetVenues();
 
   return (
-    <div className="relative bg-gray-50 px-4 pt-16 pb-20 sm:px-6 lg:px-8 lg:pt-24 lg:pb-28">
-      <div className="absolute inset-0">
-        <div className="h-1/3 bg-white sm:h-2/3" />
-      </div>
-      <div className="relative mx-auto  max-w-7xl">
+    <div className="relative bg-white px-4 pt-16 pb-20 sm:px-6 lg:px-8 lg:pt-24 lg:pb-28">
+      <div className="relative mx-auto max-w-7xl">
         <div className="text-center">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Our Venues
