746eda0c8d0337fbbe324524e6426c6f5a8ec3ba
Samuel Armitt-Fior <68110533+Sam-Archie@users.noreply.github.com>
2022-11-22 15:54:30 +0000

    style hotfix

--- pages/venues/index.tsx
+++ pages/venues/index.tsx
@@ -10,7 +10,7 @@ export default function Venues() {
   const { data: venueData } = useGetVenues();
 
   return (
-    <div className="relative bg-white px-4 pt-16 pb-20 sm:px-6 lg:px-8 lg:pt-24 lg:pb-28">
+    <div className="relative px-4 pt-16 pb-20 sm:px-6 lg:px-8 lg:pt-24 lg:pb-28">
       <div className="relative mx-auto max-w-7xl">
         <div className="text-center">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
