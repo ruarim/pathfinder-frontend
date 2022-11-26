f92065ba794cb9f0023f71e7c2bfffb619b62781
RuariM <48099261+RuariM@users.noreply.github.com>
2022-11-26 17:41:36 +0000

    implement venue search

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,6 +1,11 @@
-import React from "react";
+import { Dispatch, SetStateAction, SyntheticEvent, useState } from "react";
 import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
-export default function SearchInput() {
+
+interface SearchInputProps {
+  setSearchParam: Dispatch<SetStateAction<string>>;
+}
+
+export default function SearchInput({ setSearchParam }: SearchInputProps) {
   return (
     <div className="flex flex-1 items-center justify-center px-2 lg:ml-6 lg:justify-end">
       <div className="w-full max-w-lg lg:max-w-xs">
@@ -20,6 +25,9 @@ export default function SearchInput() {
             className="block w-full rounded-md border border-gray-300 bg-white py-2 pl-10 pr-3 leading-5 placeholder-gray-500 focus:border-indigo-500 focus:placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
             placeholder="Search"
             type="search"
+            onChange={(e: SyntheticEvent<HTMLInputElement>) =>
+              setSearchParam(e.currentTarget.value)
+            }
           />
         </div>
       </div>
