f32f955490b754d797474672c05d78e28c4ac1cf
RuariM <48099261+RuariM@users.noreply.github.com>
2022-12-17 16:29:17 +0000

    clean up

--- components/VenueMapCard.tsx
+++ components/VenueMapCard.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import timeFormatter from "../helpers/timeFormatter";
 import clsx from "clsx";
 import { useMap } from "react-map-gl";
+import { TagIcon } from "@heroicons/react/24/outline";
+import { MapPinIcon } from "@heroicons/react/20/solid";
 
 interface VenueMapCardProps {
   venue: Venue;
@@ -49,7 +51,7 @@ export default function VenueMapCard({
                       key={attribute}
                       className="bg-blue-100 p-2 rounded-md flex space-x-1"
                     >
-                      <div>{Tag}</div>
+                      <TagIcon className="w-5" />
                       <div>{attribute}</div>
                     </div>
                   );
@@ -101,45 +103,8 @@ export default function VenueMapCard({
             map.flyTo({ center: [latLong.long, latLong.lat] });
         }}
       >
-        {Pin}
+        <MapPinIcon className="w-8" />
       </div>
     </div>
   );
 }
-
-const Pin = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    className="w-8 h-8"
-  >
-    <path
-      fillRule="evenodd"
-      d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z"
-      clipRule="evenodd"
-    />
-  </svg>
-);
-
-const Tag = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-    className="w-5 h-5"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z"
-    />
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M6 6h.008v.008H6V6z"
-    />
-  </svg>
-);
--- pages/plans/index.tsx
+++ pages/plans/index.tsx
@@ -1,32 +1,36 @@
-import { BaseSyntheticEvent, useState } from "react";
+import { BaseSyntheticEvent, useEffect, useState } from "react";
 import {
   useGetAttributes,
-  useGetMapBoxLocations,
   useGetVenuesByAttributes,
 } from "../../hooks/queries";
 import clsx from "clsx";
-import VenueMapCard from "../../components/VenueMapCard";
 import Map, {
   Marker,
   NavigationControl,
   FullscreenControl,
   GeolocateControl,
-  MapRef,
   MapProvider,
+  useMap,
 } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import MapSearch from "../../components/MapSearch";
+import VenueMapCard from "../../components/VenueMapCard";
+import {
+  XMarkIcon,
+  MapPinIcon,
+  ChevronDoubleUpIcon,
+} from "@heroicons/react/24/outline";
 
 const mapboxToken = process.env.NEXT_PUBLIC_MAP_BOX_TOKEN;
+const DEFAULT_CENTER_LOCATION = {
+  lat: 51.47513029807826,
+  long: -2.591221556113587,
+};
 
 export default function Plans() {
   const [attributesParams, setAttributesSearchParams] = useState<string[]>([]);
   const [isPathModalOpen, setPathModalOpen] = useState(true);
-  const [venuesPath, setVenuesPath] = useState<string[]>([]);
-  const [centerPoint, setCenterPoint] = useState<latLong>({
-    lat: 51.47513029807826,
-    long: -2.591221556113587, //use geoLoacation from browser
-  }); //@dev create helper to initalise to browser default location
+  const [venuesPath, setVenuesPath] = useState<string[]>([]); //@dev might need venue id for db ref
   const [selectedStart, setSelectedStart] = useState<MapLocation>({
     place_name: "",
     center: [0, 0],
@@ -35,8 +39,6 @@ export default function Plans() {
     place_name: "",
     center: [0, 0],
   });
-
-  //@dev create map ref instance to pass down to components
 
   const { data: attributes } = useGetAttributes();
   const { data: venues } = useGetVenuesByAttributes(attributesParams);
@@ -76,14 +78,14 @@ export default function Plans() {
                   onClick={() => setPathModalOpen(false)}
                   className="hover:animate-pulse"
                 >
-                  {ModalOpenArrow}
+                  <ChevronDoubleUpIcon className="w-6" />
                 </div>
               ) : (
                 <div
                   onClick={() => setPathModalOpen(true)}
                   className="hover:animate-pulse rotate-180"
                 >
-                  {ModalOpenArrow}
+                  <ChevronDoubleUpIcon className="w-6" />
                 </div>
               )}
             </div>
@@ -96,7 +98,7 @@ export default function Plans() {
                     selected={selectedStart}
                     setSelected={setSelectedStart}
                   />
-                  {/* @dev make endpoint optional */}
+                  {/* @TODO make endpoint optional */}
                   <MapSearch
                     label={"End"}
                     placeholder={"Choose an ending location"}
@@ -105,8 +107,9 @@ export default function Plans() {
                   />
                 </div>
                 <div>
+                  {/* choose attributes */}
                   <div className="text-lg font-medium text-gray-900 mb-2">
-                    Pick some attributes
+                    Choose some attributes
                   </div>
                   <div className="grid grid-cols-4 w-full gap-2">
                     {attributes?.data.data.map((attribute: string) => {
@@ -147,7 +150,7 @@ export default function Plans() {
                           >
                             {name}
                             <button onClick={() => toggleVenueInPath(name)}>
-                              {Cross}
+                              <XMarkIcon className="w-6" />
                             </button>
                           </div>
                         );
@@ -164,119 +167,114 @@ export default function Plans() {
         )}
 
         {/* map box */}
-        <Map
-          id="map"
-          initialViewState={{
-            latitude: centerPoint.lat,
-            longitude: centerPoint.long,
-            zoom: 14,
-            bearing: 0,
-            pitch: 0,
-          }}
-          style={{ height: "94%", position: "absolute", zIndex: -1 }}
-          mapStyle="mapbox://styles/mapbox/streets-v12"
-          mapboxAccessToken={mapboxToken}
-        >
-          <GeolocateControl position="top-right" />
-          <FullscreenControl position="top-right" />
-          <NavigationControl position="top-right" />
-
-          {selectedStart.place_name !== "" && (
-            <Marker
-              latitude={selectedStart.center[1]}
-              longitude={selectedStart.center[0]}
-              anchor="bottom"
-            >
-              {Pin}
-            </Marker>
-          )}
-          {selectedEnd.place_name !== "" && (
-            <Marker
-              latitude={selectedEnd.center[1]}
-              longitude={selectedEnd.center[0]}
-              anchor="bottom"
-            >
-              {Pin}
-            </Marker>
-          )}
-
-          {venues?.data?.data
-            ?.sort(
-              (first: Venue, second: Venue) =>
-                second.address.latitude - first.address.latitude
-            )
-            .map((venue: Venue) => {
-              return (
-                <Marker
-                  key={venue.id}
-                  latitude={venue.address.latitude}
-                  longitude={venue.address.longitude}
-                  anchor="bottom"
-                >
-                  <VenueMapCard
-                    key={venue.id}
-                    venue={venue}
-                    venuesPath={venuesPath}
-                    toggleVenueInPath={toggleVenueInPath}
-                    latLong={{
-                      lat: venue.address.latitude,
-                      long: venue.address.longitude,
-                    }}
-                  />
-                </Marker>
-              );
-            })}
-        </Map>
+        <MapBox
+          venues={venues}
+          venuesPath={venuesPath}
+          startPoint={selectedStart}
+          endPoint={selectedEnd}
+          toggleVenueInPath={toggleVenueInPath}
+        />
       </div>
     </MapProvider>
   );
 }
-const Pin = //@dev use heroicons package
-  (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      className="w-8 h-8"
+
+interface MapBoxProps {
+  venues: VenueResponse;
+  venuesPath: string[];
+  startPoint: MapLocation;
+  endPoint: MapLocation;
+  toggleVenueInPath: (venue: string) => void;
+}
+
+export function MapBox({
+  venues,
+  venuesPath,
+  startPoint,
+  endPoint,
+  toggleVenueInPath,
+}: MapBoxProps) {
+  const { map } = useMap();
+
+  //center map on user location
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        if (map)
+          map.flyTo({
+            center: [position.coords.longitude, position.coords.latitude],
+          });
+      });
+    } else {
+      /* geolocation IS NOT available, handle it */
+      //@TODO show modal
+      console.log("geolocation not available");
+    }
+  }, [map]);
+
+  return (
+    <Map
+      id="map"
+      initialViewState={{
+        latitude: DEFAULT_CENTER_LOCATION.lat,
+        longitude: DEFAULT_CENTER_LOCATION.long,
+        zoom: 14,
+        bearing: 0,
+        pitch: 0,
+      }}
+      style={{ height: "100%", position: "absolute", zIndex: -1 }}
+      mapStyle="mapbox://styles/mapbox/streets-v12"
+      mapboxAccessToken={mapboxToken}
     >
-      <path
-        fillRule="evenodd"
-        d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z"
-        clipRule="evenodd"
-      />
-    </svg>
+      <GeolocateControl position="top-right" />
+      <FullscreenControl position="top-right" />
+      <NavigationControl position="top-right" />
+
+      {startPoint.place_name !== "" && (
+        <Marker
+          latitude={startPoint.center[1]}
+          longitude={startPoint.center[0]}
+          anchor="bottom"
+        >
+          <MapPinIcon />
+        </Marker>
+      )}
+      {endPoint.place_name !== "" && (
+        <Marker
+          latitude={endPoint.center[1]}
+          longitude={endPoint.center[0]}
+          anchor="bottom"
+        >
+          <MapPinIcon />
+        </Marker>
+      )}
+
+      {venues?.data?.data
+        ?.sort(
+          (first: Venue, second: Venue) =>
+            second.address.latitude - first.address.latitude
+        )
+        .map((venue: Venue) => {
+          return (
+            <Marker
+              key={venue.id}
+              latitude={venue.address.latitude}
+              longitude={venue.address.longitude}
+              anchor="bottom"
+            >
+              <VenueMapCard
+                key={venue.id}
+                venue={venue}
+                venuesPath={venuesPath}
+                toggleVenueInPath={toggleVenueInPath}
+                latLong={{
+                  lat: venue.address.latitude,
+                  long: venue.address.longitude,
+                }}
+              />
+            </Marker>
+          );
+        })}
+    </Map>
   );
-
-const ModalOpenArrow = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-    className="w-6 h-6"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M4.5 12.75l7.5-7.5 7.5 7.5m-15 6l7.5-7.5 7.5 7.5"
-    />
-  </svg>
-);
-
-const Cross = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-    className="w-6 h-6"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M6 18L18 6M6 6l12 12"
-    />
-  </svg>
-);
+}
