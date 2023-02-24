fc938b60a200b366b8ab72841fc27c2525d40623
RuariM <48099261+RuariM@users.noreply.github.com>
2023-02-24 16:31:58 +0000

    header clean up

--- components/Header.tsx
+++ components/Header.tsx
@@ -64,13 +64,116 @@ function MobileNavLink() {
   );
 }
 
-export default function Header() {
+function ProfileDropDown() {
   const { logout, isLoggedIn } = useAuthContext();
 
   function logoutHandler() {
-    client.post("logout");
     logout && logout();
   }
+  return (
+    <Menu as="div" className="relative ml-4 flex-shrink-0">
+      <div>
+        <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+          <span className="sr-only">Open user menu</span>
+          <img
+            className="h-8 w-8 rounded-full"
+            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            alt=""
+          />
+        </Menu.Button>
+      </div>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          {!isLoggedIn && (
+            <>
+              {" "}
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="/login"
+                    className={clsx(
+                      active ? "bg-gray-100" : "",
+                      "block px-4 py-2 text-sm text-primary/50"
+                    )}
+                  >
+                    Login
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="/register"
+                    className={clsx(
+                      active ? "bg-gray-100" : "",
+                      "block px-4 py-2 text-sm text-primary/50"
+                    )}
+                  >
+                    Register
+                  </a>
+                )}
+              </Menu.Item>
+            </>
+          )}
+          {isLoggedIn && (
+            <>
+              {/* <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={clsx(
+                      active ? "bg-gray-100" : "",
+                      "block px-4 py-2 text-sm text-primary/50"
+                    )}
+                  >
+                    Your Profile
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={clsx(
+                      active ? "bg-gray-100" : "",
+                      "block px-4 py-2 text-sm text-primary/50"
+                    )}
+                  >
+                    Settings
+                  </a>
+                )}
+              </Menu.Item> */}
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    onClick={logoutHandler}
+                    className={clsx(
+                      active
+                        ? "bg-green-400/50  cursor-pointer"
+                        : "bg-blue-400/50",
+                      "block px-4 py-2 text-sm text-primary/50"
+                    )}
+                  >
+                    Log Out
+                  </a>
+                )}
+              </Menu.Item>
+            </>
+          )}
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  );
+}
+export default function Header() {
   return (
     <Disclosure as="nav" className="bg-white shadow">
       {({ open }) => (
@@ -93,7 +196,9 @@ export default function Header() {
                   })}
                 </div>
               </div>
-
+              <div className="text-2xl font-bold absolute min-[601px]:hidden pt-4">
+                Pathfinder
+              </div>
               <div className="flex items-center lg:hidden">
                 {/* Mobile menu button */}
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-primary/50 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -106,107 +211,7 @@ export default function Header() {
                 </Disclosure.Button>
               </div>
               <div className="hidden lg:ml-4 lg:flex lg:items-center">
-                {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-4 flex-shrink-0">
-                  <div>
-                    <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      {!isLoggedIn && (
-                        <>
-                          {" "}
-                          <Menu.Item>
-                            {({ active }) => (
-                              <a
-                                href="/login"
-                                className={clsx(
-                                  active ? "bg-gray-100" : "",
-                                  "block px-4 py-2 text-sm text-primary/50"
-                                )}
-                              >
-                                Login
-                              </a>
-                            )}
-                          </Menu.Item>
-                          <Menu.Item>
-                            {({ active }) => (
-                              <a
-                                href="/register"
-                                className={clsx(
-                                  active ? "bg-gray-100" : "",
-                                  "block px-4 py-2 text-sm text-primary/50"
-                                )}
-                              >
-                                Register
-                              </a>
-                            )}
-                          </Menu.Item>
-                        </>
-                      )}
-                      {isLoggedIn && (
-                        <>
-                          <Menu.Item>
-                            {({ active }) => (
-                              <a
-                                href="#"
-                                className={clsx(
-                                  active ? "bg-gray-100" : "",
-                                  "block px-4 py-2 text-sm text-primary/50"
-                                )}
-                              >
-                                Your Profile
-                              </a>
-                            )}
-                          </Menu.Item>
-                          <Menu.Item>
-                            {({ active }) => (
-                              <a
-                                href="#"
-                                className={clsx(
-                                  active ? "bg-gray-100" : "",
-                                  "block px-4 py-2 text-sm text-primary/50"
-                                )}
-                              >
-                                Settings
-                              </a>
-                            )}
-                          </Menu.Item>
-                          <Menu.Item>
-                            {({ active }) => (
-                              <a
-                                onClick={logoutHandler}
-                                className={clsx(
-                                  active
-                                    ? "bg-green-400/50  cursor-pointer"
-                                    : "bg-blue-400/50",
-                                  "block px-4 py-2 text-sm text-primary/50"
-                                )}
-                              >
-                                Log Out
-                              </a>
-                            )}
-                          </Menu.Item>
-                        </>
-                      )}
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
+                <ProfileDropDown />
               </div>
             </div>
           </div>
@@ -216,15 +221,7 @@ export default function Header() {
               <MobileNavLink />
             </div>
             <div className="border-t border-gray-200 pt-4 pb-3">
-              <div className="flex items-center px-4">
-                <div className="flex-shrink-0">
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                </div>
-              </div>
+              <ProfileDropDown />
             </div>
           </Disclosure.Panel>
         </>
