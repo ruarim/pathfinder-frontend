9503571acdbf96b026563aaa8af7cedde0ebe5f3
RuariM <48099261+RuariM@users.noreply.github.com>
2023-02-27 11:00:11 +0000

    merge

--- components/Header.tsx
+++ components/Header.tsx
@@ -93,7 +93,6 @@ export default function Header() {
                   })}
                 </div>
               </div>
-
               <div className="flex items-center lg:hidden">
                 {/* Mobile menu button */}
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-primary/50 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
