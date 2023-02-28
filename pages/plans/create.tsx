e5a5108c85c56d4913effc5c052874bad9808c60
RuariM <48099261+RuariM@users.noreply.github.com>
2023-02-28 16:54:24 +0000

    remove log

--- pages/plans/create.tsx
+++ pages/plans/create.tsx
@@ -62,7 +62,6 @@ export default function Create() {
     setAttributesSearchParams([...params]);
   };
 
-  console.log(attributesParams);
 
   const toggleVenueInPlan = (venue: Venue) => {
     let plan = venuesPlan;
