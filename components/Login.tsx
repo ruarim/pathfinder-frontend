3e9df5f08fda0196b149825e477c78739a07d3cd
RuariM <48099261+RuariM@users.noreply.github.com>
2023-03-03 02:08:03 +0000

    make both modals equal width

--- components/Login.tsx
+++ components/Login.tsx
@@ -38,11 +38,11 @@ export default function Login() {
 
   return (
     <>
-      <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="flex min-h-full flex-col justify-center px-7 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           {/* logo here */}
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-            Sign in to your account
+            Sign in
           </h2>
         </div>
 
--- components/Register.tsx
+++ components/Register.tsx
@@ -30,7 +30,7 @@ export default function Register() {
 
   return (
     <>
-      <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="flex min-h-full flex-col justify-center px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           {/* logo here */}
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
