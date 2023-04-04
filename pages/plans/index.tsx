e22bf476c1194b341791baf83efced689ddeaeda
RuariM <48099261+RuariM@users.noreply.github.com>
2023-04-05 00:08:51 +0100

    toggle public, random public plan

--- pages/plans/index.tsx
+++ pages/plans/index.tsx
@@ -1,6 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/router";
+import client from "../../axios/apiClient";
 
 export default function Plans() {
+  const { data: randomPlan } = useQuery<
+    unknown,
+    unknown,
+    { data: { path_id: number } }
+  >(["random_venue"], () => client.get("paths/public/random"), {
+    retry: false,
+  });
+
+  const random = randomPlan?.data.path_id;
+
   const router = useRouter();
 
   const planOptions = [
@@ -25,7 +37,7 @@ export default function Plans() {
     {
       title: "Random",
       icon: "🎲",
-      href: "plans/random",
+      href: `plans/${random}`,
       bgColour: "bg-notice/50 md:bg-contrast/50",
     },
   ];
@@ -38,25 +50,25 @@ export default function Plans() {
         </p>
       </div>
       <div className="place-items-center p-12 h-screen grid md:grid-cols-2 md:pl-36 md:pr-36 md:pt-12 md:pb-36 gap-4 rounded-lg">
-        {planOptions.map((option) => {
-          return (
-            <button
-              key={option.href}
-              className="relative mt-4 my-2 px-6 py-2 font-bold text-primary group w-full h-full hover:animate-pulse"
-              onClick={() => router.push(option.href)}
-            >
-              {/*@dev make boxes alternate between bg-contrast/50 bg-notice/50  */}
-              <span
-                className={`absolute inset-0 transition duration-300 ease-out transform -translate-x-2 -translate-y-2 ${option.bgColour} group-hover:translate-x-0 group-hover:translate-y-0`}
-              ></span>
-              <span className="absolute inset-0 border-2 border-primary"></span>
-              <div className="space-x-2">
-                <span className="relative text-3xl">{option.title}</span>
-                <span className="relative text-3xl">{option.icon}</span>
-              </div>
-            </button>
-          );
-        })}
+        {random &&
+          planOptions.map((option) => {
+            return (
+              <button
+                key={option.href}
+                className="relative mt-4 my-2 px-6 py-2 font-bold text-primary group w-full h-full hover:animate-pulse"
+                onClick={() => router.push(option.href)}
+              >
+                <span
+                  className={`absolute inset-0 transition duration-300 ease-out transform -translate-x-2 -translate-y-2 ${option.bgColour} group-hover:translate-x-0 group-hover:translate-y-0`}
+                ></span>
+                <span className="absolute inset-0 border-2 border-primary"></span>
+                <div className="space-x-2">
+                  <span className="relative text-3xl">{option.title}</span>
+                  <span className="relative text-3xl">{option.icon}</span>
+                </div>
+              </button>
+            );
+          })}
       </div>
     </div>
   );
