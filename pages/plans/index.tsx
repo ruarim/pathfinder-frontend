9d6fabf7e7402389df1a4894509c7149d88173c2
RuariM <48099261+RuariM@users.noreply.github.com>
2023-04-05 17:40:06 +0100

    random hotfix

--- pages/plans/index.tsx
+++ pages/plans/index.tsx
@@ -54,26 +54,29 @@ export default function Plans() {
         </p>
       </div>
       <div className="place-items-center p-12 h-screen grid md:grid-cols-2 md:pl-36 md:pr-36 md:pt-12 md:pb-36 gap-4 rounded-lg">
-        {random &&
-          planOptions.map((option) => {
-            return (
-              <button
-                key={option.href}
-                className="relative mt-4 my-2 px-6 py-2 font-bold text-primary group w-full h-full hover:animate-pulse"
-                onClick={() => router.push(option.href)}
-              >
-                <span
-                  className={`absolute inset-0 transition duration-300 ease-out transform -translate-x-2 -translate-y-2 ${option.bgColour} group-hover:translate-x-0 group-hover:translate-y-0`}
-                ></span>
-                <span className="absolute inset-0 border-2 border-primary"></span>
-                <div className="space-x-2">
-                  <span className="relative text-3xl">{option.title}</span>
-                  <span className="relative text-3xl">{option.icon}</span>
-                </div>
-                <span>{option.subTitle}</span>
-              </button>
-            );
-          })}
+        {planOptions.map((option) => {
+          return (
+            <button
+              key={option.href}
+              className="relative mt-4 my-2 px-6 py-2 font-bold text-primary group w-full h-full hover:animate-pulse"
+              onClick={() => {
+                if (option.title == "random") {
+                  if (random) router.push(option.href);
+                } else router.push(option.href);
+              }}
+            >
+              <span
+                className={`absolute inset-0 transition duration-300 ease-out transform -translate-x-2 -translate-y-2 ${option.bgColour} group-hover:translate-x-0 group-hover:translate-y-0`}
+              ></span>
+              <span className="absolute inset-0 border-2 border-primary"></span>
+              <div className="space-x-2">
+                <span className="relative text-3xl">{option.title}</span>
+                <span className="relative text-3xl">{option.icon}</span>
+              </div>
+              <span>{option.subTitle}</span>
+            </button>
+          );
+        })}
       </div>
     </div>
   );
