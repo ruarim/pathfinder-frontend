e2e112396130a85ddef621aaeb5f8d0706cd3841
RuariM <48099261+RuariM@users.noreply.github.com>
2023-04-10 18:20:45 +0100

    small tweaks

--- pages/plans/[id].tsx
+++ pages/plans/[id].tsx
@@ -78,7 +78,6 @@ function PlanCard({ plan }: PlanCardProps) {
     : [];
   const endName = plan?.endpoint_name ? plan?.endpoint_name.split(",") : [];
   const creator = getCreator(plan.users);
-  const avg_rating = plan?.rating == undefined ? 0 : plan?.rating;
 
   const { mutateAsync: togglePublic } = useMutation<
     any,
@@ -122,64 +121,60 @@ function PlanCard({ plan }: PlanCardProps) {
             {/* Product details */}
             <div className="mx-auto mt-14 max-w-2xl sm:mt-16 lg:col-span-3 lg:row-span-2 lg:row-end-2 lg:mt-0 lg:max-w-none w-full">
               <div className="flex flex-col-reverse">
-                <div className="mt-4">
-                  <div className="space-y-1">
-                    <div className="md:flex md:justify-between">
-                      <div className="space-y-1">
-                        <h1 className="text-3xl font-bold">{plan.name}</h1>
-                        {user && isInvited(plan.users, user) && (
-                          <div className="md:flex md:space-x-2 font-bold text-lg">
-                            {plan?.start_date && (
-                              <h2>
-                                {new Date(plan?.start_date).toDateString()}
-                              </h2>
-                            )}
-                            <h2>{plan?.start_time?.substring(0, 5)}</h2>
-                          </div>
+                <div className="space-y-1">
+                  <div className="md:flex md:justify-between">
+                    <div className="space-y-1">
+                      <h1 className="text-3xl font-bold">{plan.name}</h1>
+                      {user && isInvited(plan.users, user) && (
+                        <div className="md:flex md:space-x-2 font-bold text-lg">
+                          {plan?.start_date && (
+                            <h2>{new Date(plan?.start_date).toDateString()}</h2>
+                          )}
+                          <h2>{plan?.start_time?.substring(0, 5)}</h2>
+                        </div>
+                      )}
+                    </div>
+                    <div className="md:pl-3 space-y-1">
+                      <div>
+                        {creator && (
+                          <h2 className="text-2xl flex gap-2 md:justify-end">
+                            <div className="pt-1">{creator?.username}</div>
+                            {<AvatarIcon imageUrl={creator?.avatar_url} />}
+                          </h2>
                         )}
                       </div>
-                      <div className="md:pl-3 space-y-1">
-                        <div>
-                          {creator && (
-                            <h2 className="text-2xl flex gap-2 md:justify-end">
-                              <div className="pt-1">{creator?.username}</div>
-                              {<AvatarIcon imageUrl={creator?.avatar_url} />}
-                            </h2>
-                          )}
-                        </div>
-                        <div className="flex md:justify-end">
-                          {plan.is_public === 1 ? (
-                            <button
-                              onClick={() => {
-                                if (user && isCreator(user, plan.users))
-                                  handleTogglePublic(0);
-                              }}
-                              className="text-gray-400 flex pt-1 hover:underline"
-                            >
-                              <div>Public</div>
-                              <div className="pl-1 pt-1">
-                                <LockOpenIcon className="w-4" />
-                              </div>
-                            </button>
-                          ) : (
-                            <button
-                              onClick={() => {
-                                if (user && isCreator(user, plan.users))
-                                  handleTogglePublic(1);
-                              }}
-                              className="text-gray-400 flex pt-1 hover:underline"
-                            >
-                              <div>Private</div>
-                              <div className="pl-1 pt-1">
-                                <LockClosedIcon className="w-4" />
-                              </div>
-                            </button>
-                          )}
-                        </div>
+                      <div className="flex md:justify-end">
+                        {plan.is_public === 1 ? (
+                          <button
+                            onClick={() => {
+                              if (user && isCreator(user, plan.users))
+                                handleTogglePublic(0);
+                            }}
+                            className="text-gray-400 flex pt-1 hover:underline"
+                          >
+                            <div>Public</div>
+                            <div className="pl-1 pt-1">
+                              <LockOpenIcon className="w-4" />
+                            </div>
+                          </button>
+                        ) : (
+                          <button
+                            onClick={() => {
+                              if (user && isCreator(user, plan.users))
+                                handleTogglePublic(1);
+                            }}
+                            className="text-gray-400 flex pt-1 hover:underline"
+                          >
+                            <div>Private</div>
+                            <div className="pl-1 pt-1">
+                              <LockClosedIcon className="w-4" />
+                            </div>
+                          </button>
+                        )}
                       </div>
                     </div>
-                    <Rating avg_rating={plan.rating} plan={plan} />
                   </div>
+                  <Rating avg_rating={plan.rating} plan={plan} />
                 </div>
               </div>
 
@@ -227,66 +222,6 @@ function PlanCard({ plan }: PlanCardProps) {
                   </Tab.Panel>
                 </Tab.Panels>
               </Tab.Group>
-              <div className="mt-1  pt-10">
-                <ul role="list" className="mt-4 flex items-center space-x-6">
-                  <li>
-                    <a
-                      href="#"
-                      className="flex h-6 w-6 items-center justify-center text-gray-400 hover:text-gray-500"
-                    >
-                      <span className="sr-only">Share on Facebook</span>
-                      <svg
-                        className="h-5 w-5"
-                        aria-hidden="true"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M20 10c0-5.523-4.477-10-10-10S0 4.477 0 10c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V10h2.54V7.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V10h2.773l-.443 2.89h-2.33v6.988C16.343 19.128 20 14.991 20 10z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="flex h-6 w-6 items-center justify-center text-gray-400 hover:text-gray-500"
-                    >
-                      <span className="sr-only">Share on Instagram</span>
-                      <svg
-                        className="h-6 w-6"
-                        aria-hidden="true"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="flex h-6 w-6 items-center justify-center text-gray-400 hover:text-gray-500"
-                    >
-                      <span className="sr-only">Share on Twitter</span>
-                      <svg
-                        className="h-5 w-5"
-                        aria-hidden="true"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                      >
-                        <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                      </svg>
-                    </a>
-                  </li>
-                </ul>
-              </div>
             </div>
           </div>
         </div>
@@ -652,6 +587,24 @@ function MapBox({
     return { lat: total.lat / venues.length, long: total.long / venues.length };
   };
 
+  //@dev for drawing route
+  const getPlanPoints = (
+    venues: Venue[],
+    start?: { lat?: number; long?: number },
+    end?: { lat?: number; long?: number }
+  ) => {
+    const venuePoints = venues.map((venue) => [
+      venue.address.longitude,
+      venue.address.latitude,
+    ]);
+    return [
+      [start?.lat, start?.long],
+      ...venuePoints,
+      [end?.lat, end?.long],
+    ];
+  };
+  const planPoints = getPlanPoints(venues, startpoint, endpoint);
+
   return (
     <div className="rounded-md">
       <Map
@@ -659,7 +612,7 @@ function MapBox({
         initialViewState={{
           latitude: getMidPoint(venues).lat,
           longitude: getMidPoint(venues).long,
-          zoom: 13,
+          zoom: 12,
           bearing: 0,
           pitch: 0,
         }}
